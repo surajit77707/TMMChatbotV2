38da21ff0d77acb91419796e34468a297205fc69
surajit77707 <surajitsingh77707@gmail.com>
2025-03-07 06:47:25 -0800

    Update TruthDare.py

--- Venom/modules/TruthDare.py
+++ Venom/modules/TruthDare.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 import requests
-from . import app
+from Venom import VenomX
 
 truth_api_url = "https://api.truthordarebot.xyz/v1/truth"
 dare_api_url = "https://api.truthordarebot.xyz/v1/dare"
